81ef2fccf61cb3ba07ae968d846eee1fb04f5b4f
Md Asif Islam <lionelasif@outlook.com>
2024-11-02 01:04:08 +0800

    user class

---
+++ src/User.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class User {
+	
+	private String username;
+	private String usertype; 
+	private ArrayList<Double> carbonFootprintData;
+	
+	
+	public User 
+	
+	
+	
+	
+
+}
